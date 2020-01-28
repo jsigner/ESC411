f2a38ac6ea2d22d959f79337305bf6d6c9474c80
jsigne <jens.signer@uzh.ch>
2020-01-28 22:18:50 +0100

    Chapter4,5,6

---
+++ Chapter6/Chapter6part17.py
@@ -0,0 +1,15 @@
+#def haltonsequence(n):
+i=6
+
+x=str(bin(i))[:1:-1]
+def basen(a,n):
+    q=a//n
+    p=a%n
+    if n== 0:
+        return('0')
+    elif 
+def ():
+    y=0
+    for i in range(len(x)):
+        y+=int(x[i])*base**(-i-1)
+    print(y)
